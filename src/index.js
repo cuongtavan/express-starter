bc83ee2a7335767f119002ede10621e22fd61414
cuongta <cuonginfosys@gmail.com>
2018-02-26 17:39:34 +0700

    Update port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import app from './app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 express().get('/', (req, res) => {
   res.end(app());
